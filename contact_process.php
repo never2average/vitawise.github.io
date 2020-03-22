2e6d5d681c36896bc5e7d2443f72c2c221b77302
Priyesh <priyesh.sriv2017@gmail.com>
2020-03-22 22:30:23 +0530

    Product Full Completed

--- contact_process.php
+++ contact_process.php
@@ -13,7 +13,7 @@
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message from your Sierra.";
+    $subject = "You have a message from your Vitawise.";
 
     $logo = '#';
     $link = '#';
